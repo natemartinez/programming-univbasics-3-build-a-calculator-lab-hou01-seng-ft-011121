ae10d13e99a0353d4846eff02db1327ad5ddaecd
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2020-12-20 00:17:20 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,25 +1,40 @@
 def addition(num1, num2)
-num1 + num2
+num1 + num2=sum 
+prints sum
 end
 
 def subtraction(num1, num2)
-num1-num2
+num1-num2=diff
+prints diff
 end
 
 def division(num1, num2)
-num1/num2
+num1/num2=div
+prints div
 end
 
 def multiplication(num1, num2)
-num1*num2
+num1*num2=pro
+prints pro
 end
 
 def modulo(num1, num2)
-num1%num2
+num1%num2=rem
+prints rem
 end
 
 def square_root(num)
-
+Math.sqrt(num)
 end
 
-addition ("5","4")
+addition("5","4")
+
+subtraction("5","4")
+
+division("5","4")
+
+multiplication("5","4")
+
+modulo("5","4")
+
+square_root("5","4")
